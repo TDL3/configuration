5d56c7593a66528b43144742e3479a6035189bb1
TDL3 <29535697+TDL3@users.noreply.github.com>
2022-05-27 10:28:06 +0800

    Update alias.fish

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,22 +1,20 @@
-alias cp='cp -i'
-alias df='df -h' 
+### unix ###
 alias free='free -h --giga' 
-#
-# git
-#
+alias ls='ls -F -h --color=auto'
+alias grep='grep --colour=auto'
+alias egrep='egrep --colour=auto'
+alias fgrep='fgrep --colour=auto'
+alias rg='rg --color=auto'
+
+
+### git ###
 alias gc="git commit"
+alias gca='git commit -a'
 alias gap="git add -p"
 alias gs="git status"
 alias gl="git log --pretty=format:\"%h - %an, %ar : %s\""
 alias gsig='git log --show-signature'
 
-alias ls='ls --color=auto'
-alias grep='grep --colour=auto'
-alias egrep='egrep --colour=auto'
-alias fgrep='fgrep --colour=auto'
-alias fg='fgrep --color=auto'
-alias ll='ls -lF'
-alias la='ls -AF'
-alias lsa='ls -alF'
-alias open='nautilus --new-window'
+
 alias vim='nvim'
+alias python='python3'
