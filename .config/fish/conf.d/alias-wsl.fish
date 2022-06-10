176cca53dafdee8b161ad3f7a9ef50ef1646cbd7
TDL3 <29535697+TDL3@users.noreply.github.com>
2022-06-10 14:40:26 +0800

    give git log colors

--- .config/fish/conf.d/alias-wsl.fish
+++ .config/fish/conf.d/alias-wsl.fish
@@ -10,7 +10,7 @@ abbr -ag gs 'git status -b'
 abbr -ag gc 'git commit -v'
 abbr -ag gca 'git commit -v -a'
 abbr -ag gap 'git add -p'
-abbr -ag gl 'git log --pretty format:\"%h - %an, %ar : %s\"'
+abbr -ag gl 'git log --pretty="format:%Cgreen %h %Creset - %an, %ar: %Cblue %s'
 abbr -ag gd 'git diff'
 abbr -ag gds 'git diff --stat'
 
