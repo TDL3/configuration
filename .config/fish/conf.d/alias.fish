0e34039b9490bec5203c0e864b967f4c86e52d89
TDL3 <29535697+TDL3@users.noreply.github.com>
2022-06-27 16:55:12 +0800

    Update alias.fish

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -4,6 +4,7 @@ alias ls 'ls -Fh --color=always'
 abbr tarcreate 'tar -czvf'
 abbr tarlist 'tar -ztvf'
 abbr untar 'tar -xzvf'
+abbr fif 'grep -rnw . -e'
 
 alias vim 'nvim'
 alias brewup 'brew update; and brew upgrade; and brew cleanup --prune=7'
