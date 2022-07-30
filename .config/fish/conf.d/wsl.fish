ecb4f8a0ecdf94d377dd45020db5cf224b4a6ac4
TDL3 <29535697+TDL3@users.noreply.github.com>
2022-07-30 20:08:39 +0800

    change to home dir when launch

--- .config/fish/conf.d/wsl.fish
+++ .config/fish/conf.d/wsl.fish
@@ -1,7 +1,23 @@
 abbr -ag cdd 'cd /mnt/d/Downloads'
 abbr -ag cdh 'cd ~'
+
+alias fd 'fdfind'
+alias open 'explorer.exe'
+
+function nvencode
+  /mnt/d/Program\ Files/cli/NVEncode/NVEncC64.exe $argv
+end
+
+abbr -ag cdd 'cd /mnt/d/Downloads'
+abbr -ag cdh 'cd ~'
 alias fd 'fdfind'
 alias open 'explorer.exe'
 function nvencode
   /mnt/d/Program\ Files/cli/NVEncode/NVEncC64.exe $argv
+end
+
+set -x rootdir "/mnt/c/Windows/system32"
+
+if test $rootdir = (pwd)
+  cd ~
 end
