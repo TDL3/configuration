1a3b02777a45d1f70c69e89c22e546c974c52737
TDL3 <29535697+TDL3@users.noreply.github.com>
2022-09-28 13:57:55 +0800

    fix wsl config

--- .config/fish/conf.d/wsl.fish
+++ .config/fish/conf.d/wsl.fish
@@ -1,19 +1,11 @@
-abbr -ag cdd 'cd /mnt/d/Downloads'
+abbr -ag cdd 'cd /mnt/e/Downloads'
 abbr -ag cdh 'cd ~'
 
 alias fd 'fdfind'
 alias open 'explorer.exe'
 
 function nvencode
-  /mnt/d/Program\ Files/cli/NVEncode/NVEncC64.exe $argv
-end
-
-abbr -ag cdd 'cd /mnt/d/Downloads'
-abbr -ag cdh 'cd ~'
-alias fd 'fdfind'
-alias open 'explorer.exe'
-function nvencode
-  /mnt/d/Program\ Files/cli/NVEncode/NVEncC64.exe $argv
+  /mnt/e/Program\ Files/cli/NVEncode/NVEncC64.exe $argv
 end
 
 set -x rootdir "/mnt/c/Windows/system32"
